aa3bba12d259f5a236333a13765b8d0b8b182344
Darkkrye <pierre.boudon.27@gmail.com>
2017-01-11 18:05:20 +0100

    Initial Commit.

---
+++ DKDetailsParallax/DKDetailsParallax/DKDetailsParallax.h
@@ -0,0 +1,19 @@
+//
+//  DKDetailsParallax.h
+//  DKDetailsParallax
+//
+//  Created by Pierre on 11/01/2017.
+//  Copyright © 2017 Pierre BOUDON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DKDetailsParallax.
+FOUNDATION_EXPORT double DKDetailsParallaxVersionNumber;
+
+//! Project version string for DKDetailsParallax.
+FOUNDATION_EXPORT const unsigned char DKDetailsParallaxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKDetailsParallax/PublicHeader.h>
+
+
